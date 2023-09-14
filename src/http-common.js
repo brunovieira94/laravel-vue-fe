6762483a79b48c9ae1c9c4b624fe628b4f3c0bec
Bruno Ramos <bruno.ramos@in8.com.br>
2023-09-14 16:03:06 -0300

    Improved JWT invalidation

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
+import router from './router';
 
-export default axios.create({
-    baseURL: "http://localhost:8000/api",
+const instance = axios.create({
+    baseURL: 'http://localhost:8000/api',
     headers: {
         "Content-type": "application/json"
     }
 });
+
+instance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('user');
+            router.push('/home');
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
+
+
+// import axios from "axios";
+
+// export default axios.create({
+//     baseURL: "http://localhost:8000/api",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+// });
